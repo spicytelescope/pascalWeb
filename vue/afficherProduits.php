e85b7a917b42ae59ea4f5d060b0e909748009bf1
Jérémy <rcd.jeremy@gmail.com>
2021-03-18 01:21:16 +0100

    + joli je trouve

--- vue/afficherProduits.php
+++ vue/afficherProduits.php
@@ -11,7 +11,7 @@ $labels = $products->fetch_fields();
     }
 </style>
 <div class="container">
-    <div class="card red lighten-2 z-depth-1 hoverable">
+    <div class="card red accent-2 z-depth-1 hoverable">
         <div class="card-content white-text">
             <span class="card-title">
                 <h1 class="center-align">Produits vitrines</h1>
@@ -33,12 +33,14 @@ $labels = $products->fetch_fields();
                             echo "<td>$v</td>";
                         }
 
-                        echo '<td id="tableActions"><a class="waves-effect waves-light btn green tooltipped modal-trigger" data-position="top" data-tooltip="Edit a product" href="#modal1"><i class="material-icons">edit</i></a>';
+                        echo '<td id="tableActions"><a class="waves-effect waves-light btn green tooltipped modal-trigger" 
+                        data-position="top" data-tooltip="Edit the product ' . $p['nom'] . '"
+                         href="#modal1"><i class="material-icons">edit</i></a>';
                         echo '<a class="waves-effect waves-light btn red tooltipped" data-position="top" data-tooltip="Delete a product" href="/vitrine/modele/delete.php?name=' . $p['nom'] . '&description=' . $p['description'] . '&price=' . $p['prix'] . '&code=' . $p['code'] . '"><i class="material-icons">delete</i></a></td>';
                         echo "</tr>";
-
+                        
                         // Modal
-                        echo '<div id="modal1" class="modal blue darken-1">
+                        echo '<div id="modal1" class="modal blue darken-2">
                             <div class="modal-content">
                                 <div class="container">
                                         <div class="card-content white-text">
